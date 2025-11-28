890944227969d4d7853b9fc22785436b22741fe3
noorahmed17 <noorshalbi@gmail.com>
2025-11-28 22:16:02 +0200

    FIX

--- db/pool.js
+++ db/pool.js
@@ -15,11 +15,17 @@ dotenv.config({ path: "./.env" });
 // });
 console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
+  family: 4, // forces IPv4
 });
 
 pool.on("connect", () => {
