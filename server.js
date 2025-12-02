2e27ef663d0a4df1217d3d7ed8ba4744dde98a66
hassanballeh <hassanballeh2003@gmail.com>
2025-12-02 22:03:02 +0300

    fix all .env

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const helmet = require("helmet");
 const cors = require("cors");
 const productRouter = require("./routes/productRouter");
@@ -13,7 +12,7 @@ const orderRouter = require("./routes/orderRoutes");
 const requestRouter = require("./routes/requestRouter");
 const initDB = require("./db/initDB");
 const { startCleanupJob } = require("./jobs/cleanupJobs");
-dotenv.config({ path: "./.env" });
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
